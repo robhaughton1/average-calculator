4cb90a05d95e0091e9e3d6eb0d866e4468d0c160
Robert Haughton <152531780+robhaughton1@users.noreply.github.com>
2026-03-22 01:50:12 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+numbers = []
+greater_than_average = []
+try:
+    while len(numbers) < 5:
+        raw = input("Enter a number: ")
+
+        if not raw:
+            print("Message cannot be empty.")
+            time.sleep(1)
+            continue
+
+        try:
+            user = int(raw)
+        except ValueError:
+            print("Input only numbers.")
+            time.sleep(1)
+            continue
+
+        numbers.append(user)
+
+    average = sum(numbers)/len(numbers)
+    for number in numbers:
+        if number > average:
+            greater_than_average.append(number)
+
+except Exception as e:
+    print(f"Error: {e}")
+    
+print(f"Here is your average: {average}")
+time.sleep(0.5)
+print(f"And here are the numbers greater than the average: {greater_than_average}")
