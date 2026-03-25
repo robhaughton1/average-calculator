16949a26584b64f5e48c3a3acf5ca2ce9fb76e3a
Robert Haughton <152531780+robhaughton1@users.noreply.github.com>
2026-03-24 21:24:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import time
 numbers = []
-greater_than_average = []
-try:
-    while len(numbers) < 5:
-        raw = input("Enter a number: ")
+greater_than_average = [] 
+print("Welcome to my average calculator! Insert numbers or say 'done'! :)")
+while len(numbers) < 5:
+    raw = input("Please enter a number: ")
+    if not raw:
+        print("Input cannot be empty.")
+        time.sleep(1)
+        continue
+    if raw.lower().strip() == "done":
+        if not numbers:
+                print("Insert atleast 1 number!")
+                time.sleep(1)
+                continue
+        break
 
-        if not raw:
-            print("Message cannot be empty.")
-            time.sleep(1)
-            continue
-
-        try:
-            user = float(raw)
-        except ValueError:
-            print("Input only numbers.")
-            time.sleep(1)
-            continue
-
-        numbers.append(user)
-
+    try:
+        user = float(raw)
+    except ValueError:
+        print("Input only numbers or 'done'!")
+        time.sleep(1)
+        continue
+    if user in numbers:
+        print("Select a different number please!")
+        time.sleep(1)
+        continue
+    numbers.append(user)
+if not numbers:
+    print("Insert atleast 1 number!")
+else: 
     average = sum(numbers)/len(numbers)
     for number in numbers:
         if number > average:
             greater_than_average.append(number)
-
-except Exception as e:
-    print(f"Error: {e}")
-    
-print(f"Here is your average: {average}")
-time.sleep(0.5)
-print(f"And here are the numbers greater than the average: {greater_than_average}")
+    print(f"Here is your average: {average}")
+    time.sleep(0.5)
+    print(f"Greater than average numbers: {greater_than_average}")
